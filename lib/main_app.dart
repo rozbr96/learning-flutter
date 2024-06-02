d5080f30d7a8fd53a5e54f8d977e6859d30ac96f
Rosemilson <rozbr96@gmail.com>
2024-06-01 22:00:27 -0300

    Move login screen related code to a more appropriate location

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,25 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:learning_flutter/screens/login.dart';
-import 'package:learning_flutter/utils/colors.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: primaryBackgroundColor,
-        body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage('assets/images/login-bg.jpg'),
-            ),
-          ),
-          child: const LoginScreen(),
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => const MaterialApp(home: LoginScreen());
 }
