e9187b736296544c3e60e22d7895643c335deb33
Rosemilson <rozbr96@gmail.com>
2024-06-02 14:11:44 -0300

    Refact login model

--- lib/models/provider/login.dart
+++ lib/models/provider/login.dart
@@ -1,13 +1,14 @@
-import 'package:flutter/material.dart';
+class LoginModel {
+  String _email = 'user@rebase.com';
+  String _password = 'password';
 
-class LoginModel extends ChangeNotifier {
-  String _email = '';
-  String _password = '';
-
-  String getEmail() => _email;
-  String getPassword() => _password;
   Map<String, String> getData() => {'email': _email, 'password': _password};
 
-  setEmail(String email) => {_email = email};
-  setPassword(String password) => {_password = password};
+  setEmail(String email) {
+    _email = email;
+  }
+
+  setPassword(String password) {
+    _password = password;
+  }
 }
