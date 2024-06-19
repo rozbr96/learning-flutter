1695713d8bb8a8db5b13449376c54ea5b158a371
Rosemilson <rozbr96@gmail.com>
2024-06-19 13:55:01 -0300

    Display message about empty search result

--- lib/widgets/listing/exams.dart
+++ lib/widgets/listing/exams.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:learning_flutter/models/provider/exams.dart';
+import 'package:learning_flutter/utils/colors.dart';
 import 'package:learning_flutter/widgets/cards/exam_brief.dart';
 import 'package:provider/provider.dart';
 
@@ -10,20 +12,29 @@ class ExamsListing extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemCount: context.watch<ExamsModel>().getExamsCount(),
-      itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            height: 220,
-            child: ExamBriefCard(
-              exam: context.read<ExamsModel>().getExam(index),
+    return context.watch<ExamsModel>().getExams().isEmpty
+        ? Text(
+            AppLocalizations.of(context)!.noExams,
+            style: const TextStyle(
+              color: primaryForegroundColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
             ),
-          ),
-        );
-      },
-    );
+          )
+        : ListView.builder(
+            padding: const EdgeInsets.all(8),
+            itemCount: context.watch<ExamsModel>().getExamsCount(),
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 220,
+                  child: ExamBriefCard(
+                    exam: context.read<ExamsModel>().getExam(index),
+                  ),
+                ),
+              );
+            },
+          );
   }
 }
